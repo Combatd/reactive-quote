8142e72908ba90d0a445dfca719befdd2c5d870a
Combatd <combatdstar@gmail.com>
2019-12-25 19:09:01 -0800

    Fetch REST API in getQuotes in QuoteContainer

--- src/Containers/QuoteContainer/index.js
+++ src/Containers/QuoteContainer/index.js
@@ -13,6 +13,28 @@ class QuoteContainer extends Component {
         }
     }
 
+    componentDidMount() {
+        this.getQuotes();
+    }
+
+    getQuotes = async () => {
+        try {
+            const quotes = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json' , {
+                credentials: 'include',
+                method: 'Get',
+                headers: {
+                    'Content-Type': 'applicartion/json'
+                }
+            });
+            const parsedQuotes = await quotes.json();
+            this.setState({
+                quotes: parsedQuotes.data
+            }) 
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
